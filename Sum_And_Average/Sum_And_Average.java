4b8c386344053e34556cd52d39292a253183dd91
Perry <srperry.2017@mymail.jcsu.edu>
2018-01-24 15:17:04 -0500

    first commit

---
+++ Sum_And_Average/Sum_And_Average.java
@@ -0,0 +1,22 @@
+package Sum_And_Average;
+
+public class Sum_And_Average {
+
+	public static void main(String[] args) {
+		int i;
+		int I=1;
+		int sum=1;
+		int sum1;
+		
+for(i=0;i<=99;i= i+3){
+	System.out.println(i);
+	 sum=(sum+i);
+	 I=I+1; 
+}
+System.out.println(sum);
+double average=sum/34;
+System.out.println(average);
+
+	}
+
+}
